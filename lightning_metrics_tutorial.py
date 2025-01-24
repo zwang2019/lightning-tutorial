df5035f874524b8a28977f7c492d9114552a10d6
zwang2019 <zhao.wang.unsw@gmail.com>
2025-01-25 01:47:47 +1100

    metrics tutorial

--- lightning_metrics_tutorial.py
+++ lightning_metrics_tutorial.py
@@ -10,6 +10,24 @@ from torch import nn, optim
 from torch.utils.data import random_split, DataLoader
 import torchmetrics
 from torchmetrics import Metric, Accuracy, F1Score
+from torchmetrics.functional import accuracy
+
+class MyAccuracy(Metric):
+    def __init__(self):
+        super().__init__()
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds, target) -> None:
+        preds = torch.argmax(preds, dim=1)
+        assert preds.shape == target.shape
+        self.correct += torch.sum(preds == target)
+        self.total += target.numel()
+
+    def compute(self) -> Any:
+        return self.correct.float() / self.total.float()
+
+
 
 class NN(L.LightningModule):
     def __init__(self, input_size, num_classes):
@@ -18,6 +36,11 @@ class NN(L.LightningModule):
         self.fc2 = nn.Linear(50, num_classes)
         self.accuracy = Accuracy(task='multiclass', num_classes=num_classes)
         self.f1_score = F1Score(task='multiclass', num_classes=num_classes)
+        self.my_accuracy = MyAccuracy()
+        self.training_step_y_hat = []
+        self.training_step_y = []
+        self.validation_step_y_hat = []
+        self.validation_step_y = []
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -33,12 +56,19 @@ class NN(L.LightningModule):
 
     def training_step(self, batch, batch_idx):
         loss, y_hat, y = self._common_step(batch, batch_idx)
-        self.log('train_loss', loss, prog_bar=True, sync_dist=True)
+        acc = self.my_accuracy(y_hat, y)
+        acc_2 = self.accuracy(y_hat, y)
+        self.log_dict({'train_acc': acc, 'train_acc_2': acc_2}, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.training_step_y_hat.append(y_hat)
+        self.training_step_y.append(y)
+
         return loss
 
     def validation_step(self, batch, batch_idx):
         loss, y_hat, y = self._common_step(batch, batch_idx)
         self.log('val_loss', loss, prog_bar=True, sync_dist=True)
+        self.validation_step_y_hat.append(y_hat)
+        self.validation_step_y.append(y)
         return loss
 
     def test_step(self, batch, batch_idx):
@@ -55,10 +85,32 @@ class NN(L.LightningModule):
 
 
     def on_train_epoch_end(self) -> None:
-        pass
+        y_hat = torch.cat(self.training_step_y_hat)
+        y = torch.cat(self.training_step_y)
+        # do something with all training_step outputs
+        loss = F.cross_entropy(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        f1 = self.f1_score(y_hat, y)
+        self.log_dict({'train_loss': loss, 'train_accuracy': acc, 'train_f1_score': f1}, on_epoch=True, prog_bar=True, sync_dist=True)
+        # free up the memory
+        self.training_step_y_hat.clear()
+        self.training_step_y.clear()
+        del y_hat, y
+        return None
 
     def on_validation_epoch_end(self) -> None:
-        pass
+        y_hat = torch.cat(self.validation_step_y_hat)
+        y = torch.cat(self.validation_step_y)
+        # do something with all validation_step outputs
+        loss = F.cross_entropy(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        f1 = self.f1_score(y_hat, y)
+        self.log_dict({'val_loss': loss, 'val_accuracy': acc, 'val_f1_score': f1}, on_epoch=True, prog_bar=True, sync_dist=True)
+        # free up the memory
+        self.validation_step_y_hat.clear()
+        self.validation_step_y.clear()
+        del y_hat, y
+        return None
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=0.001)
@@ -77,7 +129,7 @@ if __name__ == '__main__':
     num_classes = 10
     learning_rate = 0.001
     batch_size = 64
-    num_epochs = 3
+    num_epochs = 4
 
     # Load Data
     entire_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
